c2e44adbf68c933bddc570462f4a353855fead79
JoelNadar-TheSorceror <72254442+JoelNadar-TheSorceror@users.noreply.github.com>
2021-05-20 19:45:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var url_string = window.location.href; //window.location.href
+var url = new URL(url_string);
+try{
+    var model_url = url.searchParams.get("model_url");
+    document.body.innerHTML = `<model-viewer style='width: 100vw;height: 100vh;' auto-rotate camera-controls alt='cube' src='${model_url}'></model-viewer>`;
+}catch{
+    document.body.innerHTML = `<h1>404</h1><p>Not Found</p>`;
+}
