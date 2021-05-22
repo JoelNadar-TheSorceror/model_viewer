8a9f4eda8fcf9f31abd97c2fe9d77d243ea29ab6
JoelNadar-TheSorceror <72254442+JoelNadar-TheSorceror@users.noreply.github.com>
2021-05-22 22:08:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var url_string = window.location.href; //window.location.href
 var url = new URL(url_string);
+console.log(url_string);
 try{
     var model_url = url.searchParams.get("model_url");
     document.body.innerHTML = `<model-viewer style='width: 100vw;height: 100vh;' auto-rotate camera-controls alt='cube' src='${model_url}'></model-viewer>`;
