362d92338e9e3c3d3947b3098258cba225dfa3a2
JoelNadar-TheSorceror <72254442+JoelNadar-TheSorceror@users.noreply.github.com>
2021-06-17 19:53:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var url = new URL(url_string);
 console.log(url_string);
 try{
     var model_url = url.searchParams.get("model_url");
-    document.body.innerHTML = `<div><model-viewer style='width: 100vw;height: 100vh;background-color=#FF9800' auto-rotate camera-controls alt='cube' src='${model_url}'></model-viewer></div>`;
+    document.body.innerHTML = `<div><model-viewer style='width: 100vw;height: 100vh;background-color=black' auto-rotate camera-controls alt='cube' src='${model_url}'></model-viewer></div>`;
 }catch{
     document.body.innerHTML = `<h1>404</h1><p>Not Found</p>`;
 }
